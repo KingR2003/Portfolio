96d0c797968914970734e0d62e4502b320a620d4
Harshananda Akkasali <83390709+KingR2003@users.noreply.github.com>
2025-07-09 12:42:19 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Skills from './components/Skills';
@@ -7,33 +7,69 @@ import Experience from './components/Experience';
 import Highlights from './components/Highlights';
 import Contact from './components/Contact';
 import FadeInWhenVisible from './components/FadeInWhenVisible';
+import { useInView } from 'react-intersection-observer';
 
 function App() {
+  const [activeSection, setActiveSection] = useState('Home');
+
+  const options = { triggerOnce: false, threshold: 0.4 };
+
+  const [skillsRef, skillsInView] = useInView(options);
+  const [projectsRef, projectsInView] = useInView(options);
+  const [experienceRef, experienceInView] = useInView(options);
+  const [highlightsRef, highlightsInView] = useInView(options);
+  const [contactRef, contactInView] = useInView(options);
+
+  useEffect(() => {
+    if (contactInView) setActiveSection('Contact');
+    else if (highlightsInView) setActiveSection('Highlights');
+    else if (experienceInView) setActiveSection('Experience');
+    else if (projectsInView) setActiveSection('Projects');
+    else if (skillsInView) setActiveSection('Skills');
+    else setActiveSection('Home');
+  }, [
+    skillsInView,
+    projectsInView,
+    experienceInView,
+    highlightsInView,
+    contactInView,
+  ]);
+
   return (
     <div className="bg-gray-900 text-white">
-      <Navbar />
+      <Navbar active={activeSection} />
 
       <Hero />
 
-      <FadeInWhenVisible>
-        <Skills />
-      </FadeInWhenVisible>
+      <div ref={skillsRef}>
+        <FadeInWhenVisible>
+          <Skills />
+        </FadeInWhenVisible>
+      </div>
 
-      <FadeInWhenVisible delay={0.1}>
-        <Projects />
-      </FadeInWhenVisible>
+      <div ref={projectsRef}>
+        <FadeInWhenVisible delay={0.1}>
+          <Projects />
+        </FadeInWhenVisible>
+      </div>
 
-      <FadeInWhenVisible delay={0.2}>
-        <Experience />
-      </FadeInWhenVisible>
+      <div ref={experienceRef}>
+        <FadeInWhenVisible delay={0.2}>
+          <Experience />
+        </FadeInWhenVisible>
+      </div>
 
-      <FadeInWhenVisible delay={0.3}>
-        <Highlights />
-      </FadeInWhenVisible>
+      <div ref={highlightsRef}>
+        <FadeInWhenVisible delay={0.3}>
+          <Highlights />
+        </FadeInWhenVisible>
+      </div>
 
-      <FadeInWhenVisible delay={0.4}>
-        <Contact />
-      </FadeInWhenVisible>
+      <div ref={contactRef}>
+        <FadeInWhenVisible delay={0.4}>
+          <Contact />
+        </FadeInWhenVisible>
+      </div>
     </div>
   );
 }
